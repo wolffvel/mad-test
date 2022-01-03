9a79ec760542fefdbcc9c3e1d8ac0db532bf8ada
Pablo Wolff <pablowolff@Pablos-MacBook-Pro.local>
2022-01-03 14:07:13 -0500

    user-card: Create user card

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,13 @@ import UserCard from './components/user-card'
 function App() {
   return (
     <div className="App">
-      <UserCard />
+      <UserCard
+        title="John Doe"
+        location="Medellín, Colombia"
+        mail="jdoe@mail.com"
+        phone="555-555-555"
+        imgURL="https://randomuser.me/api/portraits/men/75.jpg"
+      />
     </div>
   )
 }
--- src/components/user-card/user-card.tsx
+++ src/components/user-card/user-card.tsx
@@ -1,11 +1,38 @@
 import React from 'react'
 import './user-card.css'
 
-const UserCard = () => {
+interface UserCardProps {
+  title: string
+  imgURL: string
+  mail: string
+  phone: string
+  location: string
+}
+
+const UserCard: React.FC<UserCardProps> = ({
+  title,
+  imgURL,
+  mail,
+  phone,
+  location,
+}) => {
   return (
-    <div className="user-card-container">
-      <h1>User Card</h1>
-    </div>
+    <article className="user-card-container">
+      <header className="user-card-header">
+        <h1 className="user-card-title">{title}</h1>
+        <div className="user-card-picture">
+          <img src={imgURL} alt="User profile picture" />
+        </div>
+      </header>
+      <div className="user-card-body">
+        <div className="user-card-body-spacer" />
+        <div className="user-card-body-content">
+          <p>{mail}</p>
+          <p>{phone}</p>
+          <p>{location}</p>
+        </div>
+      </div>
+    </article>
   )
 }
 
