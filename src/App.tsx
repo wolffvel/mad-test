8ddb80d81028445549c574dc61d7dc811552d63f
Pablo Wolff <pablowolff@Pablos-MacBook-Pro.local>
2022-01-03 12:12:03 -0500

    user-card: Add user card container styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css'
+import UserCard from './components/user-card'
 
 function App() {
   return (
     <div className="App">
-      <h1>Mad test</h1>
+      <UserCard />
     </div>
   )
 }
---
+++ src/components/user-card/user-card.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './user-card.css'
+
+const UserCard = () => {
+  return (
+    <div className="user-card-container">
+      <h1>User Card</h1>
+    </div>
+  )
+}
+
+export default UserCard
