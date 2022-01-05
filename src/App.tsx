b53ced572167060032434dd4ccd279d56ac707d2
Pablo Wolff <pablowolff@Pablos-MacBook-Pro.local>
2022-01-05 11:58:36 -0500

    add filter fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
+import Navbar from './components/nav-bar'
 import GetUsersContainer from './containers/GetUsersContainer'
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Navbar />
       <GetUsersContainer />
     </div>
   )
---
+++ src/components/nav-bar/nav-bar.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import './nav-bar.css'
+
+const Navbar: React.FC = () => {
+  const [options, toggleOptions] = useState(true)
+
+  const onBurgerButtonPress = () => {
+    toggleOptions(!options)
+  }
+
+  return (
+    <nav className="nav-container">
+      <div className="nav-controls">
+        <button className="nav-button" onClick={onBurgerButtonPress}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </div>
+      <div
+        className={options ? 'nav-mobile-options' : 'nav-mobile-options-hidden'}
+      >
+        <div className="nav-mobile-options-container">
+          <label htmlFor="userFilter">Filter users:</label>
+          <input
+            className="nav-mobile-filter-input"
+            id="userFilter"
+            type="text"
+            placeholder="Filter users"
+          />
+          <label htmlFor="filterOptions">Filter by:</label>
+          <select className="nav-mobile-filter-input">
+            <option value="name">Name</option>
+            <option value="lastName">Last Name</option>
+            <option value="email">Email</option>
+            <option value="phone">Phone</option>
+            <option value="location">Location</option>
+          </select>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
