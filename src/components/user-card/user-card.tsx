6f3ef983c024f5d1148fcb7e8a736636bad18b80
Pablo Wolff <pablowolff@Pablos-MacBook-Pro.local>
2022-01-06 16:31:39 -0500

    Add edit to fields

--- src/components/user-card/user-card.tsx
+++ src/components/user-card/user-card.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaUserEdit } from 'react-icons/fa'
 import './user-card.css'
 
 interface UserCardProps {
@@ -16,20 +17,86 @@ const UserCard: React.FC<UserCardProps> = ({
   phone,
   location,
 }) => {
+  const [cardFields, setCardFields] = useState({
+    title,
+    imgURL,
+    mail,
+    phone,
+    location,
+  })
+  const [editFields, toggleEditFields] = useState(false)
+
   return (
     <article className="user-card-container">
       <header className="user-card-header">
-        <h1 className="user-card-title">{title}</h1>
+        <div className="user-card-edit-container">
+          {!editFields && (
+            <FaUserEdit
+              onClick={() => toggleEditFields(true)}
+              className="user-card-edit"
+              size="1.5rem"
+            />
+          )}
+          {editFields && (
+            <button
+              onClick={() => toggleEditFields(false)}
+              className="user-card-edit-done"
+            >
+              Done
+            </button>
+          )}
+        </div>
+        {!editFields && <h1 className="user-card-title">{cardFields.title}</h1>}
+        {editFields && (
+          <input
+            onChange={(e) =>
+              setCardFields({ ...cardFields, title: e.target.value })
+            }
+            value={cardFields.title}
+            placeholder="Name"
+            type="text"
+          />
+        )}
         <div className="user-card-picture">
-          <img src={imgURL} alt="User profile picture" />
+          <img src={cardFields.imgURL} alt="User profile picture" />
         </div>
       </header>
       <div className="user-card-body">
         <div className="user-card-body-spacer" />
         <div className="user-card-body-content">
-          <p>{mail}</p>
-          <p>{phone}</p>
-          <p>{location}</p>
+          {!editFields && <p>{cardFields.mail}</p>}
+          {editFields && (
+            <input
+              onChange={(e) =>
+                setCardFields({ ...cardFields, mail: e.target.value })
+              }
+              value={cardFields.mail}
+              placeholder="Mail"
+              type="text"
+            />
+          )}
+          {!editFields && <p>{cardFields.phone}</p>}
+          {editFields && (
+            <input
+              onChange={(e) =>
+                setCardFields({ ...cardFields, phone: e.target.value })
+              }
+              value={cardFields.phone}
+              placeholder="Phone"
+              type="text"
+            />
+          )}
+          {!editFields && <p>{cardFields.location}</p>}
+          {editFields && (
+            <input
+              onChange={(e) =>
+                setCardFields({ ...cardFields, location: e.target.value })
+              }
+              value={cardFields.location}
+              placeholder="Location"
+              type="text"
+            />
+          )}
         </div>
       </div>
     </article>
